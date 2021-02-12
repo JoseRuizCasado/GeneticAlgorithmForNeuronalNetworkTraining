a2ccd4473f920de361eb81e2dddfe0d7b32f5d9f
JoseRuizCasado <joseruizcasado02@gmail.com>
2021-02-12 20:34:20 +0100

    Genetic Algorithm working properly

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -12,6 +12,7 @@ class GeneticNeuralNetwork(Sequential):
     """
     Generalization of Keras Sequential model
     """
+
     def __init__(self, child_weights=None):
         """
         Initialize Sequential Model and calling super class
@@ -37,21 +38,21 @@ class GeneticNeuralNetwork(Sequential):
                     input_dim=20,
                     activation='relu',
                     weights=[child_weights[0], np.zeros(16)])
-                )
-            self.add(
-                Dense(
-                 12,
-                 activation='relu',
-                 weights=[child_weights[1], np.zeros(12)])
             )
             self.add(
                 Dense(
-                 4,
-                 activation='softmax',
-                 weights=[child_weights[2], np.zeros(4)])
+                    12,
+                    activation='relu',
+                    weights=[child_weights[1], np.zeros(12)])
+            )
+            self.add(
+                Dense(
+                    4,
+                    activation='softmax',
+                    weights=[child_weights[2], np.zeros(4)])
             )
 
-    def forward_propagation(self, X_train, y_train):
+    def fitness_function(self, X_train, y_train):
         """
         Compute the fitness of the network
         :param X_train:
@@ -70,24 +71,31 @@ class GeneticNeuralNetwork(Sequential):
         :return:
         """
         self.compile(
-                      optimizer='adam',
-                      loss='categorical_crossentropy',
-                      metrics=['accuracy']
-                      )
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
         self.fit(X_train, y_train, epochs=epochs)
 
 
 # Chance to mutate weights
-def mutation(weights):
+def mutation(nn):
     """
     Mutate gen with mutator factor of 0.5
-    :param weights: weights to be mutated
-    :return:
+    :param nn: Neuronal network to perform the mutation
+    :return: mutate neuronal network
     """
-    selection = random.randint(0, len(weights) - 1)
-    mut = random.uniform(0, 1)
-    if mut >= .5:
-        weights[selection] *= random.uniform(-10, 10)
+    mutate_weights = []
+    for l in nn.layers:
+        weights = l.get_weights()[0]
+        selection = random.randint(0, len(weights) - 1)
+        mut = random.uniform(0, 1)
+        if mut >= .5:
+            weights[selection] *= random.uniform(-10, 10)
+        mutate_weights.append(weights)
+
+    mutate = GeneticNeuralNetwork(mutate_weights)
+    return mutate
 
 
 # Crossover traits between two Genetic Neural Networks
@@ -112,9 +120,9 @@ def crossover(nn1, nn2):
     # Iterate through all weights for crossover
     for i in range(0, len(nn1_weights)):
         # Get single point to split the matrix in parents based on # of cols
-        split = random.randint(0, np.shape(nn1_weights[i])[1]-1)
+        split = random.randint(0, np.shape(nn1_weights[i])[1] - 1)
         # Iterate through after a single point and set the remaining cols to nn_2
-        for j in range(split, np.shape(nn1_weights[i])[1]-1):
+        for j in range(split, np.shape(nn1_weights[i])[1] - 1):
             nn1_weights[i][:, j] = nn2_weights[i][:, j]
 
         # After crossover add weights to child
@@ -123,6 +131,18 @@ def crossover(nn1, nn2):
     # Create and return child object
     child = GeneticNeuralNetwork(child_weights)
     return child
+
+
+def selection_function(nn_list, listed_fitness):
+    """
+    Select two parent based on the selection function: fitness_i/sum(fitness_i)
+    :param nn_list: list with all the neuronal network of the actual population
+    :param listed_fitness: list with the fitness of all the neuronal networks
+    :return: parents to apply crossover
+    """
+    fitness_sum = sum(listed_fitness)
+    selection_probability = [f / fitness_sum for f in listed_fitness]
+    return random.choices(nn_list, weights=selection_probability, k=4)
 
 
 train_dataset = pd.read_csv('./data/train.csv')
@@ -141,8 +161,7 @@ y = ohe.fit_transform(y).toarray()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 # Create a List of all active GeneticNeuralNetworks
-networks = []
-pool = []
+population = []
 # Track Generations
 generation = 0
 population_size = 20
@@ -150,7 +169,7 @@ epochs = 100
 
 # Generate n randomly weighted neural networks
 for i in range(0, population_size):
-    networks.append(GeneticNeuralNetwork())
+    population.append(GeneticNeuralNetwork())
 
 # Cache Max Fitness
 max_fitness = 0
@@ -160,41 +179,50 @@ optimal_weights = []
 
 # Evolution Loop
 for epoch in range(epochs):
-    print(f'Epoch {epoch+1}/{epochs}')
+    print(f'Epoch {epoch + 1}/{epochs}')
     # Forward propagate the neural networks to compute a fitness score
-    for nn in networks:
+    fitness_list = []
+    new_generation = []
+    for nn in population:
         # Propagate to calculate fitness score
-        nn.forward_propagation(X_train, y_train)
+        nn.fitness_function(X_train, y_train)
         # Add to pool after calculating fitness
-        pool.append(nn)
-
-    # Clear for propagation of next children
-    networks.clear()
+        new_generation.append(nn)
+        fitness_list.append(nn.fitness)
 
     # Sort based on fitness
-    pool = sorted(pool, key=lambda x: x.fitness)
-    pool.reverse()
+    new_generation = sorted(new_generation, key=lambda x: x.fitness)
+    new_generation.reverse()
+    print(f'Max fitness: {new_generation[0].fitness}')
+    fitness_list.sort(reverse=True)
+    # print(f'Pop: {new_generation[0].fitness} list {fitness_list[0]}')
 
-    # Find Max Fitness and Log Associated Weights
-    for i in range(0, len(pool)):
-        # If there is a new max fitness among the population
-        if pool[i].fitness > max_fitness:
-            max_fitness = pool[i].fitness
-            print('Max Fitness: ', max_fitness)
-            # Reset optimal_weights
-            optimal_weights = []
-            # Iterate through layers, get weights, and append to optimal
-            for layer in pool[i].layers:
-                optimal_weights.append(layer.get_weights()[0])
-            print(optimal_weights)
+    # Select 4 parents
+    father1, mother1, father2, mother2 = selection_function(new_generation, fitness_list)
+    # print(f'Father1 fitness: {father1.fitness}, Mother1 fitness: {mother1.fitness}')
+    # print(f'Father2 fitness: {father2.fitness}, Mother2 fitness: {mother2.fitness}')
+    child1 = crossover(father1, mother1)
+    child1.fitness_function(X_train, y_train)
+    child2 = crossover(father2, mother2)
+    child2.fitness_function(X_train, y_train)
+    # Replace 2 nn with worst fitness with child, if better
+    new_generation.append(child1)
+    new_generation.append(child2)
+    new_generation = sorted(new_generation, key=lambda x: x.fitness)
+    new_generation.reverse()
+    del new_generation[population_size-2:population_size]
 
-    # Crossover, top 5 randomly select 2 partners for child
-    for i in range(0, 5):
-        for j in range(0, 2):
-            # Create a child and add to networks
-            temp = crossover(pool[i], random.choice(pool))
-            # Add to networks to calculate fitness score next iteration
-            networks.append(temp)
+    population = []
+    # Mutate the generation and keep if better
+    for gen in new_generation:
+        new_gen = mutation(gen)
+        new_gen.fitness_function(X_train, y_train)
+        if new_gen.fitness > gen.fitness:
+            population.append(new_gen)
+        else:
+            population.append(gen)
+
+
 
 # Create a Genetic Neural Network with optimal initial weights
 gnn = GeneticNeuralNetwork(optimal_weights)
